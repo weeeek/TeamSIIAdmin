593329efd7723ae5322c7a53299cab1dec816cc8
weeeek <407351071@qq.com>
2021-08-08 19:19:18 +0800

    东西真多

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Layout from '@/layout'
 
 Vue.use(Router)
 
@@ -8,8 +8,26 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Layout,
+      redirect: '/statis',
+      children: [
+        {
+          path: '/',
+          name: 'StatisView',
+          meta: { title: '访问统计', icon: 'dashboard', affix: true },
+          component: () => import('@/view/statis/view.vue')
+        }
+      ]
+    },
+    {
+      path: '/404',
+      component: () => import('@/views/error-page/404'),
+      hidden: true
+    },
+    {
+      path: '/401',
+      component: () => import('@/views/error-page/401'),
+      hidden: true
     }
   ]
 })
